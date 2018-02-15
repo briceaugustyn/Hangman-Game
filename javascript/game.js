64b76dd315e22b2e59962a8c69d0f8f5b8bbb45c
Brice Augustyn <baugustyn1000@gmail.com>
2018-02-14 19:32:05 -0800

    added function shuffle

--- javascript/game.js
+++ javascript/game.js
@@ -4,6 +4,8 @@ console.log(a)
 
 // 1 Define global variable
 
+var init=0
+var round=1
 var revealed_name="_ _ _ _ _ _ _"
 var wrong_guess= 0
 var number_of_guess_remaining= 0
@@ -24,8 +26,26 @@ var star_wars = {
 }
 
 
+
+
 //--3 Find randomely the name to be discovered (cannot be the one allready used previsously)--
 
+// this function will shuffle an array
+function shuffle (array) {
+    var i=0
+    var j=0
+    var temp =null
+    alert("array")
+    for (i = array.length -1 ; i > 0; i-=i) {
+        j=Math.floor(Math.random() * (i +1))
+        temp = array [i]
+        array[i]=array[j]
+        array[j]=temp
+        //alert(array[0]+"random")
+    }
+        
+}
+    
 
 //--4 Display the _ _ _ _ _ _ _ based on the name to be guess--
 
@@ -33,11 +53,26 @@ var star_wars = {
 //--5 Key event to listen to the letters that player is typing--
 
 document.onkeyup = function(event) {
+    // if first time playing init the number of round shuffle the list of name for star wars
+    if (init==0){
+        //alert("init")
+        var T=star_wars.name
+        shuffle(T)
+        round=prompt("how many round do you want to play ?")
+        display_round(round)
+        display_init("Game started")
+        shuffle(T)
+        init=1
+    }
     var k = event.key;
     letter_allready_guessed=k
     check(k,0);
-    display(revealed_name)
-    display2(wrong_guess)
+
+    if (round==0){
+        alert("game over")
+        init=0
+    }
+    //alert(T[0]+"hello")
     //eval(kkeys[k]);
   };
 
@@ -62,12 +97,17 @@ function check (key,index){
         name_string=name_string.replaceAt(n, "-")
         revealed_name=revealed_name.replaceAt(n, key)
         alert(name_string + " result " + (n+1)); 
+        display (revealed_name)
         }
         else {
             wrong_guess++
-            //alert("wrong"+ wrong_guess)
+            round--
+            display2(wrong_guess)
+            display_round(round)
+            alert("wrong"+ wrong_guess)
         }    
 }
+
 
 
 //--7 As the user guesses the correct letters, reveal them: m a d o _  _ a.--
@@ -75,16 +115,28 @@ function display (revealed_name){
 document.getElementById("word-blanks").innerHTML = revealed_name;
 //alert("dispay1")
 }
-
 //--8 Display number-of-guess-remaining, letter-allready-guessed, leter-guessed, win, losses--
 function display2 (wrong_guess){
 //alert("display2"+wrong_guess)
+
 document.getElementById("wrong-guesses").innerHTML = wrong_guess;
-document.getElementById("number_of_guess_remaining").innerHTML = number_of_guess_remaining;
-document.getElementById("letter_allready_guessed").innerHTML = letter_allready_guessed;
-document.getElementById("leter_guessed").innerHTML = leter_guessed;
-document.getElementById("win-counter").innerHTML = win;
-document.getElementById("losses").innerHTML = losses;
+
+// document.getElementById("number_of_guess_remaining").innerHTML = number_of_guess_remaining;
+// document.getElementById("letter_allready_guessed").innerHTML = letter_allready_guessed;
+// document.getElementById("leter_guessed").innerHTML = leter_guessed;
+// document.getElementById("win-counter").innerHTML = win;
+// document.getElementById("losses").innerHTML = losses;
     }
 
+    function display_init (init){
+        
+        document.getElementById("game-started").innerHTML = init;
+        
+            }
 
+    function display_round (round){
+        
+        document.getElementById("guesses-left").innerHTML = round;
+        //document.getElementById("number_of_guess_remaining").innerHTML = round2;
+        
+            }
